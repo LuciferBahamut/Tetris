4bc3a1d5a649e2327a75d61bdac4af990d5081bf
Chauvin Chloé <Chloe@localhost.localdomain>
2020-03-12 14:10:19 +0100

    [FIX] coding style

--- src/display_help.c
+++ src/display_help.c
@@ -6,21 +6,7 @@
 */
 
 #include "tetris.h"
-
-#define USAGE "Usage:  ./tetris [options]\nOptions:\n"
-#define OPTIONS " --help\t\t\tDisplay this help\n -L --level={num}\t"
-#define OPTIONS1 "Start Tetris at level num (def: 1)\n -l --key-left={K}\t"
-#define OPTIONS2 "Move the tetrimino LEFT using the K key (def: left arrow)\n "
-#define OPTIONS3 "-r --key_right={K}\tMove the tetrimino RIGHT using the K key"
-#define OPTIONS4 " (def: right arrow)\n -t --key-turn={K}\tTURN the tetrimino"
-#define OPTIONS5 " clockwise 90d using the K key (def: top arrow)\n -d --key-"
-#define OPTIONS6 "drop={K}\tDROP the tetrimino using the K key (def: down "
-#define OPTIONS7 "arrow)\n -q--key-quit={K}\tQUIT the game using the K key "
-#define OPTIONS8 "(def: 'q' key)\n -p --key-pause={K}\tPAUSE/RESTART the "
-#define OPTIONS9 "game using the K key (def: space bar)\n --map-size="
-#define OPTIONSA "{row,col}\tSet the numbers of rows and columns of the map "
-#define OPTIONSB "(def: 20,10)\n -w --without-next\tHide next tetrimino "
-#define OPTIONSC "(def: false)\n -D --debug\t\tDebug mode (def: false)\n"
+#include "help.h"
 
 void display_help(void)
 {
